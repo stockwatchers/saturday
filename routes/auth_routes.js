d8eb60e5abb9839db16243d15f34d5933d416054
Kris <kristopher_hughes@yahoo.com>
2016-01-31 21:56:41 -0800

    authentication

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,25 +1,54 @@
+const Profile = require( __dirname + '/../models/profile');
 const express = require('express');
-const mongoose = require('mongoose');
-
-//Authorization
-const jwt = require('jsonwebtoken');
-const basicHTTP = require( __dirname + '/../lib/basic_http');
-
-//models
-var Profile = require( __dirname + '/../models/profile');
-
-//Middleware
-const bodyParser = require('body-parser').json();
-const handleDBError = require( __dirname + '/../lib/handle_db_error');
+const jsonParser = require('body-parser').json();
+const handleError = require( __dirname + '/../lib/handle_db_error');
+const basicHTTP = require(__dirname + '/../lib/basic_http');
 
 var authRouter = module.exports = exports = express.Router();
 
-//Log In with existing account
-authRouter.get('/signin' , basicHTTP, (req , res) => {
+//Signup
+authRouter.post('/signup', jsonParser, (req, res) => {
+  var newProfile = new Profile();
+  //Username must be entered and password must have length of 8
+  if(!((req.body.username || '').length && (req.body.password || '').length > 7)) {
+    return res.status(400).json({msg: 'Invalid username or password'});
+  }
+  //Check if username is taken
+  Profile.count({'authentication.username' : req.body.username}, function(err, count) {
+    if(err) {
+      console.log(err);
+      return res.status(400).json({msg: 'Sorry, we are having technical difficulties.'});
+    }
+    if (count > 0) {
+      return res.status(401).json({msg: 'Username already exists'});
+    }
 
+    //Assign parts of new profile in database
+    newProfile.authentication.username = req.body.username;
+    newProfile.hashPassword(req.body.password);
+    newProfile.save((err, data) => {
+      if(err) return handleError(err, res);
+      res.status(200).json({token: data.generateToken()});
+    });
+  });
 });
 
-//Log In with new account
-authRouter.post('/signin' , bodyParser, basicHTTP, (req , res) => {
-
+//Signin
+authRouter.get('/signin', basicHTTP, (req, res) => {
+  Profile.findOne({'authentication.username' : req.basicHTTP.username}, (err, user) => {
+    if(err) {
+      console.log(err);
+      //Database error
+      return res.status(401).json({msg: 'Sorry, we are having technical difficulties.'});
+    }
+    //No User
+    if(!user) return res.status(401).json({msg: 'NONE SHALL PASS!'});
+    //Password not matching
+    if(!user.comparePassword(req.basicHTTP.password)) {
+    	return res.status(401).json({msg: 'Password Mismatch'});
+    }
+    //Give verified user a token
+    res.status(200).json({token: user.generateToken()});
+  });
 });
+
