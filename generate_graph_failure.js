d0abfe9431bb0e5eee3fd3dcad3485d1a4d9d811
Joshua Ho <ho.joshua4@gmail.com>
2016-02-03 17:28:45 -0800

    Working graphs mfffff

--- generate_graph_failure.js
+++ generate_graph_failure.js
@@ -6,7 +6,6 @@ $('.stockWatch').click(function() {
 		//WHERE WE UPDATE GRAPHS AND STUFF
 		updateStockInfo( processStockData(data) );
 		console.log('Graph updated');
-
 		//This is where data is coming from, to be used by all other processes.
   });
 });
@@ -38,35 +37,38 @@ function processStockData ( dataInc ) {
 	var stockName; //How can we get this?
 	var stockObject = {};
 	dataInc = JSON.parse(dataInc);
+	// console.log(dataInc);
 	//Data we need once - Fed to title and stats on front page
 	stockObject.Symbol = dataInc.query.results.quote[dataInc.query.results.quote.length-1].Symbol;
 	stockObject.Open = dataInc.query.results.quote[dataInc.query.results.quote.length-1].Open;
 	stockObject.Volume = dataInc.query.results.quote[dataInc.query.results.quote.length-1].Volume;
 	stockObject.Close = dataInc.query.results.quote[dataInc.query.results.quote.length-1].Close;
+	stockObject.quotes = [];
 	// stockObject.Quotes = dataInc.query.results.quote;
 	var quoteData = [];
+
 	for (var i = 0; i < dataInc.query.results.quote.length; i++) {
 		var quoteJSON = {
-			date: dataInc.query.results.quote[i].Date,
+			date: dataInc.query.results.quote[i].Date 	,
 			close: dataInc.query.results.quote[i].Close
 		}
-		quoteData.push(quoteJSON);
+		quoteData.push(JSON.stringify(quoteJSON));
+		//Date is loaded
 	}
+		//Date disappears
 	stockObject.quotes = quoteData;   	//[ {obj:obj,key:val} , {obj:obj,key:val}]
+	console.log('Data Processed');
+	return stockObject;
 	// //Data to calculate -> Needs to be fed to the graph as an array of data
 	// stockDate = data.query.results.quote[0].Date;
 	// stockLow = data.query.results.quote[0].Low;
 	// stockHigh = data.query.results.quote[0].High;
-	console.log('Data Processed');
-	return stockObject;
+
 }
 
-function generateLineChart(container , quoteData) {
-
-	$('#stockGraph').remove();
-
+function generateLineChart($container , quoteData) {
   var margin = {top:20, right:20, bottom:40, left:60};
-  var width = 800 - margin.left - margin.right;
+  var width = 700 - margin.left - margin.right;
   var height = 300 - margin.top - margin.bottom;
 
 	//Callback
@@ -74,27 +76,23 @@ function generateLineChart(container , quoteData) {
 
   var x = d3.time.scale().range([0, width]);
   var y = d3.scale.linear().range([height, 0]);
+  var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
+  var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
 
-	//Adjust number of ticks on graph
-	//Months
-  var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(4);
-	//Value
-	var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
-
-  var valueLine = d3.svg.line().x(function(d){return x( d.date );}).y(function(d){return y( d.close );});
-  var svg = d3.select(container).append('svg')
+  var valueLine = d3.svg.line().x(function(d){return x( 10 );}).y(function(d){return y( 5 );});
+  var svg = d3.select($container).append('svg')
 	          .attr('width', width + margin.left + margin.right)
 	          .attr('height', height + margin.top + margin.bottom)
-						.attr('id', 'stockGraph')
-						.append('g')
+	      		.append('g')
 	          .attr('transform', 'translate (' + margin.left + ',' + margin.top + ')');
 
+						//All data is processed for graph generation  here
 	quoteData.forEach(function(d) {
-			d.date = parseDate(d.date);
-			d.close = parseFloat(d.close);
+		d = JSON.parse(d);
+		d.date = parseDate(d.date);
+		d.close = parseFloat(d.close);
 	});
-
-	var area = d3.svg.area()
+  var area = d3.svg.area()
 	           .x(function(d) {
 	              return x(d.date);
 	          })
@@ -102,38 +100,34 @@ function generateLineChart(container , quoteData) {
 	          .y1(function(d) {
 	              return y(d.close);
 	          });
+x.domain(d3.extent(quoteData, function(d) { return d.date; }));
+y.domain([0, d3.max(quoteData, function(d) { return d.close;})]);
 
-	//Provide ranges to X and Y
-	x.domain(d3.extent(quoteData, function(d) { return d.date; }));
-	y.domain([0, d3.max(quoteData, function(d) { return d.close;})]);
-
-	//Used to render graph data
-	//Draw Lines
-	svg.append('path')
+svg.append('path')
 	.attr('class', 'line')
 	.attr('d', valueLine(quoteData));
-	//Draw Area on Graph
-	svg.append('path')
+
+/*svg.append('path')
 	.datum(quoteData)
 	.attr('class', 'area')
-	.attr('d', area);
-	//etc.
-	svg.append('g')
+	.attr('d', area);*/
+
+svg.append('g')
 	.attr('class', 'x axis')
 	.attr('transform', 'translate(0,' + height + ')')
 	.call(xAxis);
 
-	svg.append('g')
+svg.append('g')
 	.attr('class', 'y axis')
 	.call(yAxis);
 
-	svg.append('text')
+svg.append('text')
 	.attr('transform', 'translate(' + (width / 2) + ',' + (height + margin.bottom) + ')')
 	.style('text-anchor', 'middle')
 	.attr('class', 'shadow')
 	.text('Date');
 
-	svg.append('text')
+svg.append('text')
 	.attr('transform', 'rotate(-90)')
 	.attr('y', -50)
 	.attr('x', margin.top - (height / 2))
@@ -141,6 +135,7 @@ function generateLineChart(container , quoteData) {
 	.style('text-anchor', 'end')
 	.attr('class', 'shadow')
 	.text('Price ($)');
+
 };
 
 
