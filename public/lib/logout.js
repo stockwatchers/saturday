dfdb4975c12bd34f514d0042dac8ad877bce9402
James Mason <jmason@alumni.reed.edu>
2016-02-04 20:55:30 -0800

    Improved styling.

--- public/lib/logout.js
+++ public/lib/logout.js
@@ -1,11 +1,7 @@
 function delete_cookie(name) {
   document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
-$("#logout1").click(function() {
-	delete_cookie('token');
-  window.location.href = window.location.href.substr(0, window.location.href.lastIndexOf('/'));
-});
-$("#logout2").click(function() {
+$("#logout").click(function() {
 	delete_cookie('token');
   window.location.href = window.location.href.substr(0, window.location.href.lastIndexOf('/'));
 });
--- public/lib/scripts.js
+++ public/lib/scripts.js
@@ -1,37 +1,35 @@
 /* This function slides the sidebar on the page */
 
     $(".glyphicon-arrow-left").click(function() {
-        $("#sidebar-wrapper").css("margin-left", "-340px").css('transition-duration','1s');;
+        $("#sidebar-wrapper").css("left", '0px').css('transition-duration','1s');
     });
 
     $(".openPortBtm p").click(function() {
-        $("#sidebar-wrapper").css("margin-left", "-340px").css('transition-duration','1s');;
+        $("#sidebar-wrapper").css("left", '0px').css('transition-duration','1s');
     });
 
     $(".openPortBtm p").on("swipeleft",function(){
-      $("#sidebar-wrapper").css("margin-left", "-340px").css('transition-duration','1s');;
-    });
-
-    $("#viewProfile").click(function(){
-      $("#sidebar-wrapper").css("margin-left", "-340px").css('transition-duration','1s');;
+      $("#sidebar-wrapper").css("left", '0px').css('transition-duration','1s');
     });
 
 /* This function slides the sidebar off the page */
 
     $(".openGraphBtm .glyphicon-arrow-right").click(function() {
-        $("#sidebar-wrapper").css("margin-left", "80px").css('transition-duration','1s');;
+        $("#sidebar-wrapper").css("left", ($(window).width() + 'px')).css('transition-duration','1s');
     });
 
     $(".openGraphBtm p").click(function() {
-        $("#sidebar-wrapper").css("margin-left", "80px").css('transition-duration','1s');;
+        $("#sidebar-wrapper").css("left", ($(window).width() + 'px')).css('transition-duration','1s');
     });
 
     $(".openPortBtm p").on("swipe",function(){
-      $("#sidebar-wrapper").css("margin-left", "80px").css('transition-duration','1s');;
+      $("#sidebar-wrapper").css("left", ($(window).width() + 'px')).css('transition-duration','1s');
     });
-
     $("#viewGraph").click(function(){
-      $("#sidebar-wrapper").css("margin-left", "80px").css('transition-duration','1s');;
+      $("#sidebar-wrapper").css("left", ($(window).width() + 'px')).css('transition-duration','1s');
+    });
+    $('.stockWatch').click(function() {
+      $("#sidebar-wrapper").css("left", ($(window).width() + 'px')).css('transition-duration','1s');
     });
 
 /* This function opens the logout menu on the graph page */
@@ -41,14 +39,4 @@
     });
     $("#hamClick").click(function() {
         $("#hideMenu").toggleClass("a b");
-    });
-
-/* This function opens the logout menu on the profile page */
-    $(function() {
-      $("#hideMenuMain").addClass("bMain");
-    });
-    $("#hamClickMain").click(function() {
-        $("#hideMenuMain").toggleClass("aMain bMain");
-        $(".stockIn").toggleClass("slideR", 600);
-        $(".stockInIcon").toggleClass("hide", 200);
     });
